b92d990cf50a5408b7a78397ac492233df3d6e2d
Lorenzo Bucci <lorenzo.bucci@stud.unifi.it>
2021-01-25 16:50:54 +0100

    Changed types to unsigned short

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ int main() {
 
     // GENERATING STRINGS
 
-    int stringLengthA = 61367;
-    int stringLengthB = 62789;
+    unsigned short stringLengthA = 44367;
+    unsigned short stringLengthB = 44789;
 
     string str("abcdefghijklmnopqrstuvwxyz");
 
@@ -27,7 +27,7 @@ int main() {
     // SEQUENTIAL ALGORITHM
 
     auto **ptr = new unsigned short *[stringLengthA + 1];
-    for (int i = 0; i <= stringLengthA; i++)
+    for (unsigned short i = 0; i <= stringLengthA; i++)
         ptr[i] = new unsigned short[stringLengthB + 1];
 
     cout << "Starting sequential algorithm..." << endl;
@@ -39,7 +39,7 @@ int main() {
     cout << "Sequential edit distance result: " << sequentialResult << endl;
     cout << "Sequential execution time in s : " << timeElapsed.count() << endl;
 
-    for (int i = 0; i <= stringLengthA; i++)
+    for (unsigned short i = 0; i <= stringLengthA; i++)
         delete ptr[i];
     delete[] ptr;
 
@@ -47,7 +47,7 @@ int main() {
 
     for (unsigned short subMatrixSize = 10; subMatrixSize <= 1000; subMatrixSize+=100) {
         ptr = new unsigned short *[stringLengthA + 1];
-        for (int i = 0; i <= stringLengthA; i++)
+        for (unsigned short i = 0; i <= stringLengthA; i++)
             ptr[i] = new unsigned short[stringLengthB + 1];
 
         cout << endl << "Starting parallel algorithm..." << endl;
@@ -59,7 +59,7 @@ int main() {
         cout << "Parallel edit distance result: " << parallelResult << endl;
         cout << "Parallel execution time in s: " << timeElapsed.count() << " with sub-matrices size: " << subMatrixSize << endl;
 
-        for (int i = 0; i <= stringLengthA; i++)
+        for (unsigned short i = 0; i <= stringLengthA; i++)
             delete ptr[i];
         delete[] ptr;
     }
